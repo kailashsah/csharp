31c73d186b41d1cde85b8776521460f09de607d1
kailashs <kailash.patisah@ivycomptech.com>
2023-08-02 19:28:13 +0530

    Wpf.UIMemory deconstruction of window & viewModel

---
+++ WPF/Wpf.UIMemory/GameWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf.UIMemory
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class GameWindow : Window
+    {
+        private readonly int _windowIndex;
+        dynamic listContact;
+
+        public GameWindow(int windowIndex)
+        {
+            _windowIndex = windowIndex;
+            //Methodx();
+            InitializeComponent();
+           
+        }
+        ~GameWindow()
+        {
+
+            Debug.WriteLine($"Deconstruction {_windowIndex}", GetType().Name);
+        }
+
+        //public int WindowIndex => _windowIndex;
+
+        public void Methodx()
+        {
+            listContact = Enumerable.Repeat(new Contact(), 5000);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //DataContext = this; // give binding error of WindowIndex property
+            DataContext = null;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public class Contact
+    {
+        private string name = "abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz";
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+
+}
---
+++ WPF/Wpf.UIMemory/GameWindowVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.UIMemory
+{
+    public class GameWindowVM
+    {
+        public int WindowIndex { get; set; }
+        ~GameWindowVM()
+        {
+            Debug.WriteLine($"deconstruction {WindowIndex}", GetType().Name);
+        }
+    }
+    
+}
