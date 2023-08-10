5e8be29abd52e0c4fe61e9ec65c444bb437fc400
kailashs <kailash.patisah@ivycomptech.com>
2023-08-10 20:44:09 +0530

    behavior

---
+++ WPF/1_BASICS/BehaviorTextBox/TextChangeBehavior.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BehaviorScrollBar
+{
+    public class TextChangeBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            TextBox txtBox = AssociatedObject as TextBox;
+            txtBox.TextChanged += txt_TextChanged;
+            if (string.IsNullOrEmpty(txtBox.Text))
+            {
+                txtBox.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                txtBox.Background = new SolidColorBrush(Colors.Red);
+            }
+
+        }
+        void txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtBox = AssociatedObject as TextBox;
+            txtBox.TextChanged += txt_TextChanged;
+            if (string.IsNullOrEmpty(txtBox.Text))
+            {
+                txtBox.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                txtBox.Background = new SolidColorBrush(Colors.Red);
+            }
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            TextBox txtBox = AssociatedObject as TextBox;
+            txtBox.TextChanged -= txt_TextChanged;
+
+        }
+    }
+}
