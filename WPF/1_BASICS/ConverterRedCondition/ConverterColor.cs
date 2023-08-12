65d9f3a7a805c4380e571e28c9b9a8812e2f2cf8
kailashs <kailash.patisah@ivycomptech.com>
2023-08-12 16:30:29 +0530

    converter color

---
+++ WPF/1_BASICS/ConverterRedCondition/ConverterColor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace array_items_color
+{
+    class ConverterColor : IValueConverter
+    {
+        int previousInt = 0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            //return int.Parse((string)value) > 18 ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Green);
+            //if (int.Parse((string)value) > 18)
+
+            if (((int)value) < previousInt)
+            {
+                previousInt = (int)value;
+                return "Red";
+            }
+            else
+            {
+                previousInt = (int)value;
+                return "Green";
+            }
+        }
+    
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF/1_BASICS/ConverterRedCondition/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace array_items_color
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<int> Items => new ObservableCollection<int>{ 100, 20, 50, 10, 30, 90, 88, 92  };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
