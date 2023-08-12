ba23cb7316d2226f5ca63105a68d4dcd2bd3b3ef
kailashs <kailash.patisah@ivycomptech.com>
2023-08-12 14:56:09 +0530

    ANIMATIONS

---
+++ WPF/1_BASICS/ANIMATIONS/TransformsDemo1/Ex.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TransformsDemo
+{
+    /// <summary>
+    /// Interaction logic for Ex.xaml
+    /// </summary>
+    public partial class Ex : Window
+    {
+        public Ex()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void animatedRectangle_Loaded(object sender, RoutedEventArgs e)
+        {
+            //myStoryboard.Begin();
+
+            Storyboard res1 = (Storyboard)this.Resources["myst"];
+            //Object res2 = (res1 as Storyboard).Children[0];
+
+            res1.Begin();
+
+
+        }
+    }
+}
---
+++ WPF/1_BASICS/ANIMATIONS/TransformsDemo1/doubleAnimationFrm.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TransformsDemo
+{
+    /// <summary>
+    /// Interaction logic for doubleAnimationFrm.xaml
+    /// </summary>
+    public partial class doubleAnimationFrm : Window
+    {
+        public doubleAnimationFrm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard res1 = (Storyboard)this.Resources["Storyboard1"];
+            Object res2 = (res1 as Storyboard).Children[0];
+            
+            res1.Begin();
+        }
+
+        private void ButtonEnd_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard res1 = (Storyboard)this.Resources["Storyboard1"];
+            Object res2 = (res1 as Storyboard).Children[0];
+            res1.Stop();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void angleAni_Completed(object sender, EventArgs e)
+        {
+            //MessageBox.Show("a");
+        }
+
+        private void angleAni_CurrentStateInvalidated(object sender, EventArgs e)
+        {
+            //MessageBox.Show("CurrentStateInvalidated");
+        }
+
+        private void angleAni_CurrentGlobalSpeedInvalidated(object sender, EventArgs e)
+        {
+            //MessageBox.Show("CurrentGlobalSpeedInvalidated");
+        }
+    }
+}
