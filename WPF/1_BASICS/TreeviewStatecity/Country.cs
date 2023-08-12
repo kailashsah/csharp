9fe19917a559f143aca6b9590bf57736f63a3f0a
kailashs <kailash.patisah@ivycomptech.com>
2023-08-12 16:07:28 +0530

    treeview

---
+++ WPF/1_BASICS/TreeviewStatecity/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statecity_treeview
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public ObservableCollection<State> States { get; set; }
+    }
+
+    public class State
+    {
+      public string Name { get; set; }
+      public ObservableCollection<State> States { get; set; }
+
+    }
+    public class City
+    {
+        public string Name { get; set; }
+        
+
+    }
+}
---
+++ WPF/1_BASICS/TreeviewStatecity/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statecity_treeview
+{
+    public class ViewModel
+    {
+        private ObservableCollection<Country> countries;
+
+        public ObservableCollection<Country> Countries
+        {
+            get { return countries; }
+            set { countries = value; }
+        }
+
+        public ViewModel()
+        {
+            countries = new ObservableCollection<Country>();
+            ObservableCollection<State> states_india = new ObservableCollection<State>();
+            ObservableCollection<State> cities_Maharastra = new ObservableCollection<State>();
+            cities_Maharastra.Add(new State() { Name = "Mumbai" });
+            cities_Maharastra.Add(new State() { Name = "Pune" });
+            cities_Maharastra.Add(new State() { Name = "Nagpur" });
+            cities_Maharastra.Add(new State() { Name = "Thane" });
+            ObservableCollection<State> cities_karnataka = new ObservableCollection<State>();
+            cities_karnataka.Add(new State() { Name = "Bangalore" });
+            cities_karnataka.Add(new State() { Name = "Mysore" });
+            cities_karnataka.Add(new State() { Name = "Bidar" });
+            cities_karnataka.Add(new State() { Name = "Belgaum" });
+
+
+            states_india.Add( new State() { Name = "Karnataka", States = cities_karnataka  });
+            states_india.Add(new State() { Name = "Maharastra", States = cities_Maharastra });
+            countries.Add(new Country() { Name = "India", States = states_india });
+        }
+    }
+}
