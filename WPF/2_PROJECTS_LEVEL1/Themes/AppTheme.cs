ffd9ab6a0999ad2cf2b790d390442d0099dfe0d6
kailashs <kailash.patisah@ivycomptech.com>
2023-08-12 19:24:56 +0530

    themes

---
+++ WPF/2_PROJECTS_LEVEL1/Themes/AppTheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Themes
+{
+    public class AppTheme
+    {
+        public static void ChangeTheme(Uri uriTheme)
+        {
+            ResourceDictionary resDict = new ResourceDictionary() { Source = uriTheme};
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(resDict);
+        }
+    }
+}
