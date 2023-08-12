085020b38bf1d2ea9113a787e878818b9e97773a
kailashs <kailash.patisah@ivycomptech.com>
2023-08-12 14:59:35 +0530

    triggers

---
+++ WPF/1_BASICS/DataNProperyMultiTrigger/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace StylesDemo
+{
+    public class ViewModel
+    {
+        private bool isSuccess;
+
+        public bool IsSuccess
+        {
+            get { return isSuccess; }
+            set { isSuccess = value; }
+        }
+
+        private bool isPassed;
+
+        public bool IsPassed
+        {
+            get { return isPassed; }
+            set { isPassed = value; }
+        }
+
+
+    }
+
+
+}
