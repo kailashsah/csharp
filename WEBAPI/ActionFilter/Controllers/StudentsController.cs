0009ec91415651799b9bf87d99a2d2f05e2e7ecb
kailashs <kailash.patisah@ivycomptech.com>
2023-08-19 18:38:11 +0530

    action filter

---
+++ WEBAPI/ActionFilter/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Filters;
+
+namespace WebApplication1.Controllers
+{
+    [Example ("Controller student", 0)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly string[] students = { "aaa", "bbb" };
+        
+        [Example("Method Get", -1)]
+        public IActionResult Get()
+        {
+            return Ok(students);
+        }
+
+
+        [HttpGet("{index:int}")]
+        public ActionResult GetStudent(int index)
+        {
+            try
+            {
+                return Ok(students[index]);
+
+            }
+            catch (Exception ex)
+            {
+                //return StatusCode( StatusCodes.Status501NotImplemented, ex.Message);
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("{find}")]
+        public ActionResult GetStudentQuery(int stu)
+        {
+            return Ok("query handler");
+        }
+    }
+}
---
+++ WEBAPI/ActionFilter/Filters/ExampleAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Filters
+{
+    public class ExampleAttribute : Attribute, IActionFilter, IOrderedFilter
+    {
+        public string attrib { get; set; }
+
+        public int Order { get; set; }
+
+        public ExampleAttribute(string attribute, int order)
+        {
+            this.attrib = attribute;
+            this.Order = order;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Trace.WriteLine($" {this.attrib} - after example");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Trace.WriteLine($" {this.attrib} - before example");
+        }
+    }
+}
