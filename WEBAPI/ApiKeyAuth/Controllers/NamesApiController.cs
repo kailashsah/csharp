0422f153fd3e4b00005e18a6c8988803cca43ca2
kailashs <kailash.patisah@ivycomptech.com>
2023-08-20 20:08:50 +0530

    ApiKeyAuth

---
+++ WEBAPI/ApiKeyAuth/Controllers/NamesApiController.cs
@@ -0,0 +1,24 @@
+﻿using ApiKeyAuth.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiKeyAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesApiController : ControllerBase
+    {
+        private readonly string[] names = {"Tom","Parth" };
+        [ApiKeyAuth]
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            return Ok(names);
+        }
+    }
+}
---
+++ WEBAPI/ApiKeyAuth/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiKeyAuth.Filters
+{
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string apiKey = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // get value from header
+            bool res = context.HttpContext.Request.Headers.TryGetValue(apiKey, out var keyFmHeader);
+            if (!res)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            // get value from appsetting
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var keyFmConfiguration = configuration.GetValue<string>(apiKey);
+
+            // compare
+            if(keyFmHeader!= keyFmConfiguration)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
