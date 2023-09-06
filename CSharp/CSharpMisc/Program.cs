ff879c2c4a12950ed5b95b235f65df7b3aee5ead
kailash <kailashpati86@gmail.com>
2023-09-06 13:37:38 +0530

    ThreadStatic

---
+++ CSharp/CSharpMisc/Program.cs
@@ -0,0 +1,23 @@
+﻿using CSharpMisc;
+using System;
+using System.Threading;
+
+namespace ConsoleApp.ThreadStatic
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            new ThreadStaticEx().Run();
+               
+                
+            Console.WriteLine("Hello World!");
+        }// main
+
+
+    }//class
+}
+
+    
+
---
+++ CSharp/CSharpMisc/ThreadStaticEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpMisc
+{
+    public class ThreadStaticEx
+    {
+        [ThreadStatic] public static int i = 0;
+        public void Run()
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    i++;
+                    Console.WriteLine("Thread A: {0}", i); // Uses one instance of the i variable.
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    i++;
+                    Console.WriteLine("Thread B: {0}", i); // Uses another instance of the i variable.
+                }
+            }).Start();
+        }
+    }
+}
+
