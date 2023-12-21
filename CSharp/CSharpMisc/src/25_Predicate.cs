c15d112ec6cbc08fc83e78eecd4da3b749b07688
kailash <kailashpati86@gmail.com>
2023-12-21 16:59:09 +0530

    func, delegate, Func<>

---
+++ CSharp/CSharpMisc/src/25_Predicate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1. Predicate - Predicate is the delegate like Func and Action delegates. It represents a method containing a set of criteria and checks whether the passed parameter meets those criteria. 
+
+        A predicate delegate methods must take one input parameter and return a boolean - true or false.
+
+ */
+namespace CSharpMisc
+{
+    public class PredicateClass
+    {
+        bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+
+        public void RunMethod()
+        {
+            //1.
+            Predicate<string> isUpper = IsUpperCase;
+            bool result = isUpper("hello world!!");
+
+            Console.WriteLine(result);
+
+            //2.
+            //An anonymous method can also be assigned to a Predicate delegate type as shown below.
+            Predicate<string> isUpperV2 = delegate (string s) { return s.Equals(s.ToUpper()); };
+            bool result2 = isUpperV2("hello world!!");
+
+            //3.
+            //A lambda expression can also be assigned to a Predicate delegate type as shown below.
+            Predicate<string> isUpperV3 = s => s.Equals(s.ToUpper());
+            bool result3 = isUpperV3("hello world!!");
+        }
+    }
+
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        new PredicateClass().RunMethod(); 
+    //        Console.WriteLine("program ends");
+    //    }
+    //}
+
+}
---
+++ CSharp/CSharpMisc/src/27_Func_vs_delegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1. Func<> vs delegate -
+        both Func<string,string> and delegate string convertMethod(string) would be capable of holding the same method definitions whether they be methods, anonymous methods, or lambda expressions.
+
+    2. If you want the delegate to have some special name that gives more definition of what that delegate should do (beyond simple Action, Predicate, etc) then creating your own delegate is always an option.
+*/
+
+namespace CSharpMisc
+{
+    public class DelegateClass
+    {
+        // you can define your own delegate for a nice meaningful name, but the
+        // generic delegates (Func, Action, Predicate) are all defined already
+        public delegate string ConvertedMethod(string value);
+
+        public void RunMethod()
+        {
+            //1.
+            // both work fine for taking methods, lambdas, etc.
+            Func<string, string> convertedMethod = s => s + ", Hello!";
+            Console.WriteLine(convertedMethod("convertedMethod"));
+
+            //2.
+            ConvertedMethod convertedMethod2 = s => s + ", Hello!";
+            Console.WriteLine(convertedMethod2("convertedMethod2"));
+        }
+    };
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new DelegateClass().RunMethod();
+            Console.WriteLine("program ends");
+        }
+    }
+
+}
