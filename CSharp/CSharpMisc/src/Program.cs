c7db8d540799a5c676b82e34048a578bb2789c1b
kailash <kailashpati86@gmail.com>
2023-12-21 16:13:53 +0530

    hold console output using Read()

--- CSharp/CSharpMisc/src/Program.cs
+++ CSharp/CSharpMisc/src/Program.cs
@@ -9,35 +9,12 @@ namespace ConsoleApp.ThreadStatic
 {
     public class Program
     {
-
         //public static void Main(string[] args)
         //{
-        //    Misc.RunListToString();
-
-        //    ParallelMethodCall.RunParallelMethodCall();
-        //    // 4.
-        //    //TimeConsumed.RunTimeConsumed();
-
-        //    // 3.
-        //    //ImmutableObject immObj = new ImmutableObject("Kailash", 38);
-        //    //immObj.Age = 40; // cannot be assigned to -- readonly
-
-        //    // 2.
-        //    //DestructorDemo.RunDestructorDemo();
-        //    //ObjectSize.RunObjectSize();
-
-        //    // 1.
-        //    //new ThreadStaticEx().Run();
-        //    //
-
-
         //    Console.WriteLine("program ends");
-        //}// main
-
-
-
-
-    }//class
+        //    Console.Read();// holds the console output if it exit immediatley after showing output
+        //}
+    }
 
 }
 
