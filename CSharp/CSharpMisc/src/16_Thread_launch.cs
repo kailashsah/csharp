f1fa22ce045678b51375c45191ebf12831d62a51
kailash <kailashpati86@gmail.com>
2023-12-25 14:31:04 +0530

    thread launches

--- CSharp/CSharpMisc/src/16_Thread_launch.cs
+++ CSharp/CSharpMisc/src/16_Thread_launch.cs
@@ -11,31 +11,73 @@ namespace CSharpMisc
 {
     public class ThreadLaunch
     {
+        //1.
         public void ThreadMethod(string param)
         {
             Console.WriteLine("ThreadMethod :" + param);
-            Console.WriteLine($"ThreadMethod :{param}" ); // ThreadMethod :param
+            Console.WriteLine($"ThreadMethod :{param}" ); // $ - string interpolation
+            Console.WriteLine("ThreadMethod :{0}", param); // ThreadMethod :param
             Console.WriteLine(@"ThreadMethod :{param}" ); // ThreadMethod :{param}
+                                                          // @ - verbatim string, prints string as is 
         }
 
-        public void Run()
+        public void RunV1()
         {
+            //using anonymous function using lambda
             Thread thread = new Thread(()=> ThreadMethod("param"));
             thread.Start();
             thread.Join();
             thread.Join(100); // 100ms timeout 
                                 // multiple join() no exception
         }
-    }
-    public class Program
-    {
-        public static void Main(string[] args)
+        
+        //2.
+        public void ThreadMethodV2(object param)
         {
-            // 1.
-            new ThreadLaunch().Run();
-            //
-            Console.WriteLine("program ends");
-        }// main
+            //An object that contains data for the thread procedure.
+            Console.WriteLine("ThreadMethod :" + param);
+            Console.WriteLine($"ThreadMethod :{param}"); // ThreadMethod :param
+            Console.WriteLine(@"ThreadMethod :{param}"); // ThreadMethod :{param}
+        }
+        public void RunV2()
+        {
+            Console.WriteLine("");
+            Thread thread = new Thread(ThreadMethodV2); // we need param as 'object' class .. ThreadMethod(object param)
+           
+            thread.Start("param"); // supply params here
+            thread.Join();
+            thread.Join(100); // 100ms timeout 
+        }
+
+        //3.
+        public void RunV3_using_parameterized_threadstart()
+        {
+            Console.WriteLine("");
+            //1.
+            Thread thread = new Thread(new ParameterizedThreadStart( ThreadMethodV2));
+            //2.
+            //Thread thread = new Thread(new ThreadStart(ThreadMethodV2));// ThreadStart class for method with null paramas only.
+
+            thread.Start("param");
+            thread.Join();
+            thread.Join(100); // 100ms timeout 
+        }
+        public void Run()
+        {
+            RunV1();
+            RunV2();
+            RunV3_using_parameterized_threadstart();
+        }
     }
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        // 1.
+    //        new ThreadLaunch().Run();
+    //        //
+    //        Console.WriteLine("program ends");
+    //    }// main
+    //}
 
 }
