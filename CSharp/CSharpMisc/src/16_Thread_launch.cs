2dc98a382e862cd8b6d8de5b44541843c49e6199
kailash <kailashpati86@gmail.com>
2023-12-25 14:01:58 +0530

    invoke thread

---
+++ CSharp/CSharpMisc/src/16_Thread_launch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1. call thread for method with parameter
+ */
+namespace CSharpMisc
+{
+    public class ThreadLaunch
+    {
+        public void ThreadMethod(string param)
+        {
+            Console.WriteLine("ThreadMethod :" + param);
+            Console.WriteLine($"ThreadMethod :{param}" ); // ThreadMethod :param
+            Console.WriteLine(@"ThreadMethod :{param}" ); // ThreadMethod :{param}
+        }
+
+        public void Run()
+        {
+            Thread thread = new Thread(()=> ThreadMethod("param"));
+            thread.Start();
+            thread.Join();
+            thread.Join(100); // 100ms timeout 
+                                // multiple join() no exception
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1.
+            new ThreadLaunch().Run();
+            //
+            Console.WriteLine("program ends");
+        }// main
+    }
+
+}
