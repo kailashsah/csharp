fe6fddbbbf5e483b6ae19b8b9242757644bc2e0c
kailash <kailashpati86@gmail.com>
2024-05-17 11:50:27 +0530

    find insert position

---
+++ CSharp/CSharpMisc/src_questions/15_search_insert_position_binary_search.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/*
+    1. Search Insert Position
+
+        Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+        You must write an algorithm with O(log n) runtime complexity. 
+
+        Example 1:
+        Input: nums = [1,3,5,6], target = 5
+        Output: 2
+        
+        Example 2:
+        Input: nums = [1,3,5,6], target = 2
+        Output: 1
+
+        Example 3:
+        Input: nums = [1,3,5,6], target = 7
+        Output: 4
+
+*/
+
+namespace CSharpMisc
+{
+    public class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            return BinarySearch(nums, 0, nums.Length-1, target);
+        }
+
+        public int BinarySearch(int[] nums, int left, int right, int target)
+        {
+            if (left > right)
+                return left; // If the search space is empty, return the insertion position
+
+            int mid  = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[mid] < target)
+                return BinarySearch(nums, mid+1, right, target); // Search right half
+            else //(nums[mid] > target)
+                return BinarySearch(nums, left, mid-1, target); // Search left half
+        }
+
+
+        public void Run()
+        {
+            int[] nums = {1, 3, 5, 6};
+            Console.WriteLine(SearchInsert(nums, 5));//2
+
+            Console.WriteLine(SearchInsert(nums, 2));//1
+            Console.WriteLine(SearchInsert(nums, 7));//4
+        }
+
+        //static void Main(String[] args)
+        //{
+        //    new SearchInsertPosition().Run();
+
+        //}
+    }
+}
