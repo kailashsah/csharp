c2a0a34ff55ce67e832fc6b6d635a3213c4f31c3
kailash <kailashpati86@gmail.com>
2024-05-18 13:28:01 +0530

    class to try random code

---
+++ CSharp/CSharpMisc/src/99_code_practice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1.  
+*/
+
+namespace CSharpMisc
+{
+
+    public class RunTemp123
+    {
+        public void Run()
+        {
+            
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    new RunTemp123().Run();
+        //    Console.WriteLine("program ends");
+        //}
+    }
+}
