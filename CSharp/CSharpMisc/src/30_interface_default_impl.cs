9fe19a34af524e2a3d13d204d0df95d9043447d3
kailash <kailashpati86@gmail.com>
2024-05-18 10:27:12 +0530

    interface default implementation

---
+++ CSharp/CSharpMisc/src/30_interface_default_impl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1.  
+
+
+*/
+
+namespace CSharpMisc
+{
+    public interface IDefaultImpl
+    {
+        //Interfaces can now provide default method implementations.
+        void Display();
+
+        void PrintIt() => Console.WriteLine("PrintIt");
+    }
+
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        new Rectangle().RunMultiCastDelegate();
+    //        Console.WriteLine("program ends");
+    //    }
+    //}
+
+}
