2e1302fb42698783615c6a94303fa4895df6b97a
kailash <kailashpati86@gmail.com>
2024-07-07 12:10:16 +0530

    other reads

---
+++ CSharp/CSharpMisc/src/97_other_reads.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    1. init - The init keyword defines an accessor method in a property or indexer. An init-only setter assigns a value to the property or the indexer element only during object construction. 
+        
+        An init enforces immutability, so that once the object is initialized, it can't be changed. An init accessor enables calling code to use an object initializer to set the initial value. 
+
+        As a contrast, an auto-implemented property with only a get setter must be initialized by calling a constructor. A property with a private set accessor can be modified after construction, but only in the class.
+
+    2. indexer
+        https://www.geeksforgeeks.org/c-sharp-indexers/ 
+
+ */
+
+namespace CSharpMisc
+{
+
+
+}
