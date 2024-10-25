b0fffca40bc3c2ad87321c0c8280ebadcf110f81
kailash <kailashpati86@gmail.com>
2024-10-25 21:34:37 +0530

    comment

--- MiscProjs/BouncyCastleEncryption/Program.cs
+++ MiscProjs/BouncyCastleEncryption/Program.cs
@@ -39,7 +39,7 @@ namespace ECCurves
                 var iv = "00112233445566778899AABBCCDDEEFF00";
                 byte[] nonce = new byte[8];
                 byte[] bytes = Encoding.ASCII.GetBytes(iv); 
-                Array.Copy(bytes, nonce, 8); // here not using Convert.FromHexString(iv)
+                Array.Copy(bytes, nonce, 8); // here not using Convert.FromHexString(iv) 
 
                 var keyGen = GeneratorUtilities.GetKeyGenerator("AES128");
                 KeyParameter keyParam = new KeyParameter(keyGen.GenerateKey());
